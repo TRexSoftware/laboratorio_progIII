982f9698b595f8162260c8265a023007dfd35b5b
flaviopifiator <you@example.com>
2015-10-28 18:27:08 -0300

    registro

---
+++ reservar/controladores/controller.Registro.php
@@ -0,0 +1,10 @@
+<?php
+require_once("../inc.includes.php");
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$email = $_POST['email'];
+$usuario = $_POST['user'];
+$pass = $_POST['password'];
+
+
+?>
