e17e73b169545499194d07debf51307b6574e336
Gabriel Acosta <acostadariogabriel@gmail.com>
2015-11-01 02:47:25 -0300

    La consulta de hoteles ahora tiene en cuenta el nombre, ciudad y localidad

--- reservar/modelos/model.Hotels.php
+++ reservar/modelos/model.Hotels.php
@@ -3,7 +3,7 @@
 class MHotels {
     function buscar_hoteles($name) {
         global $db;
-        return $db->consultar("SELECT * FROM hotel WHERE nom_hotel='$name' or localidad='$name'");
+        return $db->consultar("SELECT * FROM hotel WHERE nom_hotel='$name' or localidad='$name' or provincia='$name'");
     }
 }
 ?>
