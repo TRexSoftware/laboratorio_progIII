be63c45d3127b1771168c5128a52dde3c63fdfa3
Gabriel Acosta <acostadariogabriel@gmail.com>
2015-11-01 03:21:08 -0300

    Mensage de error cuando no se ingresa texto en input

--- reservar/controladores/controller.Hoteles.php
+++ reservar/controladores/controller.Hoteles.php
@@ -6,24 +6,28 @@ class R_Controller {
         $tp->prepare();
         $tp->gotoBlock("_ROOT");
         $mhotels = new MHotels();
-        $result = $mhotels->buscar_hoteles($_POST['hotel']);
-        if($result['found']) {
-            
-            foreach($result['result'] as $r) {
-                $tp->newblock("hotels");
-                $tp->assign("name", $r['nom_hotel']);
-                $tp->assign("prov", $r['provincia']);
-                $tp->assign("local", $r['localidad']);
-                $tp->assign("calle", $r['calle']);
-                $tp->assign("ncalle", $r['nro_calle']);
-                $tp->assign("tel", $r['telefono']);
-                $tp->assign("precio", $r['precio_persona']);
-            }
+        if($_POST['hotel'] == "") {
+            $tp->newblock("error");
+            $tp->assign("msg", "No se ha ingresado nada");
         } else {
-            $tp->newblock("no_hotels");
-            $tp->assign("hotel", $_POST['hotel']);
+            $result = $mhotels->buscar_hoteles($_POST['hotel']);
+            if($result['found']) {
+            
+                foreach($result['result'] as $r) {
+                    $tp->newblock("hotels");
+                    $tp->assign("name", $r['nom_hotel']);
+                    $tp->assign("prov", $r['provincia']);
+                    $tp->assign("local", $r['localidad']);
+                    $tp->assign("calle", $r['calle']);
+                    $tp->assign("ncalle", $r['nro_calle']);
+                    $tp->assign("tel", $r['telefono']);
+                    $tp->assign("precio", $r['precio_persona']);
+                }
+            } else {
+                $tp->newblock("no_hotels");
+                $tp->assign("hotel", $_POST['hotel']);
+            }
         }
-        
         echo $tp->getOutputContent();
     }
 }
